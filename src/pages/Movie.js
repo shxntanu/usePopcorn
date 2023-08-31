30b961f574bdb0a3717719eca03d1d3d773112dc
Shantanu Wable <shantanuwable2003@gmail.com>
2023-08-31 20:56:17 +0530

    Update

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,9 +1,22 @@
-function Movie() {
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import NavBar from "../components/NavBar";
+import Search from "../components/Search";
+import Numresults from "../utils/Numresults";
+
+function Movie({ movies }) {
+    const [query, setQuery] = useState("");
+    const { userId } = useParams();
+
     return (
         <div>
-            
+            <NavBar>
+                <Search query={query} setQuery={setQuery} />
+                <Numresults movies={movies} />
+            </NavBar>
         </div>
-    )
+    );
 }
 
-export default Movie
+export default Movie;
