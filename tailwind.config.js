62dbf118f3129d511ef8bd6ba3f82903b285a9a3
Shantanu <shantanuwable2003@gmail.com>
2023-08-31 15:18:11 +0530

    Installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    colors: {
+      'primary': '#6741d9',
+      'primary-light': '#7950f2',
+      'text': '#dee2e6',
+      'text-dark' : '#adb5bd',
+      'background-100': '#343a40',
+      'background-500': '#2b3035',
+      'background-900': '#212529',
+      'red' : '#fa5252',
+      'red-dark' : '#e03131'
+    }
+  },
+  plugins: [],
+}
+
