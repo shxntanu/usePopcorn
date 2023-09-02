2b72a2c0552361ecf4c833305fdb88cd7ec8fd07
Shantanu <shantanuwable2003@gmail.com>
2023-09-02 21:25:20 +0530

    Edited object parameters

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,11 +3,11 @@ import Movie from "./Movie";
 export default function MovieList({ movies, onSelectMovie }) {
     return (
         <ul className="list list-movies">
-            {movies?.map((movie) => (
+            {movies.results?.map((movie) => (
                 <Movie
                     movie={movie}
                     onSelectMovie={onSelectMovie}
-                    key={movie.imdbID}
+                    key={movie.id}
                 />
             ))}
         </ul>
