9f6eb53f053f26494e31a6109106171dbb910d48
Shantanu <shantanuwable2003@gmail.com>
2023-09-02 22:18:14 +0530

    update

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,55 +1,59 @@
 import { useState, useEffect } from "react";
+const sampleData = require('../constants/sampleData.json')
 
-export function useMovies(query){ 
-  const KEY = "c7c3c0f9";
+export function useMovies(query) {
+    const API_KEY = "d1b83dd054786999cdeab1df570feb46";
 
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
+    const [movies, setMovies] = useState(sampleData);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState("");
 
-  useEffect(
-    function () {
-    //   callback?.();
-      const controller = new AbortController();
+    useEffect(
+        function () {
+            //   callback?.();
+            const controller = new AbortController();
 
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
+            async function fetchMovies() {
+                try {
+                    setIsLoading(true);
+                    setError("");
 
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
+                    const res = await fetch(
+                        `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${API_KEY}`,
+                        { signal: controller.signal }
+                    );
 
-          if (query === "") return;
+                    if (query === "") return;
 
-          if (!res.ok) throw new Error("Something went wrong");
+                    if (!res.ok) throw new Error("Something went wrong");
 
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not Found");
+                    const data = await res.json();
 
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          // console.error(err.message);
+                    console.log(data)
+                    if (data.results === [])
+                        throw new Error("Movie not Found");
 
-          if (err.name !== "AbortError") {
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-      //   handleCloseMovie();
-      fetchMovies();
+                    setMovies(data.Search);
+                    setError("");
+                } catch (err) {
+                    // console.error(err.message);
 
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
-  );
+                    if (err.name !== "AbortError") {
+                        setError(err.message);
+                    }
+                } finally {
+                    setIsLoading(false);
+                }
+            }
+            //   handleCloseMovie();
+            fetchMovies();
 
-  return { movies, isLoading, error };
+            return function () {
+                controller.abort();
+            };
+        },
+        [query]
+    );
+
+    return { movies, isLoading, error };
 }
