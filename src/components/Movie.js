1d8cb0947eba4d95333d09e35ad9e3ede31849f7
Shantanu <shantanuwable2003@gmail.com>
2023-09-02 21:25:08 +0530

    Edited Object parameters

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,12 +1,12 @@
 export default function Movie({ movie, onSelectMovie }) {
     return (
-        <li onClick={() => onSelectMovie(movie.ID)}>
-            <img src={movie.Poster} alt={`${movie.Title} poster`} />
-            <h3>{movie.Title}</h3>
+        <li onClick={() => onSelectMovie(movie.id)}>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`${movie.title} poster`} />
+            <h3>{movie.title}</h3>
             <div>
                 <p>
                     <span>🗓</span>
-                    <span>{movie.Year}</span>
+                    <span>{movie.release_date}</span>
                 </p>
             </div>
         </li>
