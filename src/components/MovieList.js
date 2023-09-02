11d3b149761fb920e2b7a531b971ecc34f43ec6a
Shantanu <shantanuwable2003@gmail.com>
2023-09-02 22:17:22 +0530

    Changed key names

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,7 +3,7 @@ import Movie from "./Movie";
 export default function MovieList({ movies, onSelectMovie }) {
     return (
         <ul className="list list-movies">
-            {movies.results?.map((movie) => (
+            {movies.results.map((movie) => (
                 <Movie
                     movie={movie}
                     onSelectMovie={onSelectMovie}
