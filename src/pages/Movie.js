f8e9c32b03f925d1379e4e61c29f5be15c28b7db
Shantanu Wable <shantanuwable2003@gmail.com>
2023-09-03 19:59:09 +0530

    Created a basic structure of movie page

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,20 +1,65 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLocation } from "react-router";
 
 import NavBar from "../components/NavBar";
 import Search from "../components/Search";
 import Numresults from "../utils/Numresults";
+import Main from "../layout/Main";
+import MovieBox from "../components/MovieBox";
 
-function Movie({ movies }) {
+function Movie() {
     const [query, setQuery] = useState("");
-    const { userId } = useParams();
+    const [movie, setMovie] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    const location = useLocation();
+
+    const { selectedID } = location.state;
+
+    const API_KEY = "d1b83dd054786999cdeab1df570feb46";
+
+    const {
+        title,
+        id,
+        poster_path,
+        vote_average,
+        release_date,
+        overview,
+        runtime,
+    } = movie;
+
+    useEffect(
+        function () {
+            async function getMovieDetails() {
+                setIsLoading(true);
+                const res = await fetch(
+                    `https://api.themoviedb.org/3/movie/${selectedID}?api_key=${API_KEY}`
+                );
+                const data = await res.json();
+                // console.log(data);
+                setMovie(data);
+                setIsLoading(false);
+            }
+            getMovieDetails();
+        },
+        [selectedID]
+    );
 
     return (
-        <div>
+        <div className="details">
             <NavBar>
                 <Search query={query} setQuery={setQuery} />
-                <Numresults movies={movies} />
+                {/* <Numresults movies={movies} /> */}
             </NavBar>
+            <Main>
+                <MovieBox>
+                    <img
+                    className="h-auto max-w-xl rounded-lg shadow-xl dark:shadow-gray-800 mx-auto"
+                        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                        alt={`Poster of ${movie}`}
+                    />
+                </MovieBox>
+            </Main>
         </div>
     );
 }
