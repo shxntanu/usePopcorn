7ff33e5470f0c82aad2c6784176fddbb6a6956fd
Shantanu <shantanuwable2003@gmail.com>
2023-09-08 22:20:30 +0530

    Removed unnecessary components

--- src/components/MovieBox.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export default function MovieBox({ children }) {
-    return (
-        <div className="movieBox">
-            {children}
-        </div>
-    );
-}
--- src/layout/Main.js
+++
@@ -1,3 +0,0 @@
-export default function Main({ children }) {
-    return <main className="main">{children}</main>;
-}
