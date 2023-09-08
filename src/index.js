a7a8e93ff164b9ef8163ac54e216fffa57d3136c
Shantanu <shantanuwable2003@gmail.com>
2023-09-08 22:20:08 +0530

    Changed route path from '/home' to '/'

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ export default function App() {
         <div>
             <Router>
                 <Routes>
-                    <Route path="/home" element={<Home />} />
+                    <Route path="/" element={<Home />} />
                     <Route path="/movie" element={<Movie />} />
                 </Routes>
             </Router>
