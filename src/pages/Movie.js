1eff634ea7ebb06c622ecfff6642f01640408388
Shantanu Wable <shantanuwable2003@gmail.com>
2023-09-09 01:48:24 +0530

    Added similar movies to the Movie page

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -5,12 +5,16 @@ import NavBar from "../components/NavBar";
 import Search from "../components/Search";
 import Badge from "../components/Badge";
 import Numresults from "../utils/Numresults";
-import axios from "axios";
+import SuggestedMovie from "../components/SuggestedMovie";
+import Loader from "../components/Loader";
+
+const sampleMovieData = require("../constants/sampleMovieData.json");
 
 function Movie() {
     const [query, setQuery] = useState("");
-    const [movie, setMovie] = useState({});
-    const [isLoading, setIsLoading] = useState(false);
+    const [movie, setMovie] = useState(sampleMovieData);
+    const [similarMovies, setSimilarMovies] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     const location = useLocation();
 
@@ -20,13 +24,10 @@ function Movie() {
 
     const {
         title,
-        id,
         poster_path,
-        vote_average,
         backdrop_path,
         release_date,
         overview,
-        runtime,
         genres,
     } = movie;
 
@@ -41,31 +42,28 @@ function Movie() {
 
     useEffect(
         function () {
-            async function getMovieDetails() {
-                setIsLoading(true);
-                const res = await fetch(
-                    `https://api.themoviedb.org/3/movie/${selectedID}?api_key=${API_KEY}`
-                );
+            try {
+                async function getMovieDetails() {
+                    var movieDetailRes = await fetch(
+                        `https://api.themoviedb.org/3/movie/${selectedID}?api_key=${API_KEY}`
+                    );
 
-                // const url =
-                //     `https://api.themoviedb.org/3/movie/${selectedID}?language=en-US`;
-                // const options = {
-                //     method: "GET",
-                //     headers: {
-                //         accept: "application/json",
-                //         Authorization:
-                //             `Bearer ${API_KEY}`,
-                //     },
-                // };
+                    const movieDetailData = await movieDetailRes.json();
 
-                // const res = await fetch(url, options)
+                    const similarMoviesRes = await fetch(
+                        `https://api.themoviedb.org/3/movie/${selectedID}/recommendations?language=en-US&page=1&api_key=${API_KEY}`
+                    );
+                    const similarMoviesData = await similarMoviesRes.json();
 
-                const data = await res.json();
-                // console.log(data);
-                setMovie(data);
-                setIsLoading(false);
+                    // console.log(movieDetailData);
+                    setMovie(movieDetailData);
+                    setSimilarMovies(similarMoviesData);
+                    setIsLoading(false);
+                }
+                getMovieDetails();
+            } catch (error) {
+                alert(error);
             }
-            getMovieDetails();
         },
         [selectedID]
     );
@@ -77,33 +75,58 @@ function Movie() {
                 {/* <Numresults movies={movies} /> */}
             </NavBar>
             <div className="main">
-                <div className="movieBox" style={backgroundStyle}>
-                    <div className="flex flex-row items-center mt-20 mx-20">
-                        <img
-                            className="object-fill h-min max-w-xl rounded-lg shadow-xl dark:shadow-gray-800"
-                            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-                            alt={`Poster of ${movie}`}
-                        />
-                        <div className="flex flex-col ml-20 space-y-5">
-                            <p className="logo body text-6xl">{title}</p>
-                            <div className="flex flex-row space-x-2">
-                                {genres &&
-                                    genres.map(({ id, name }) => {
-                                        return <Badge content={name} />;
-                                    })}
+                {isLoading && <Loader />}
+                {!isLoading && (
+                    <div className="movieBox" style={backgroundStyle}>
+                        <div className="flex flex-row items-center mt-20 mx-20">
+                            <img
+                                className="object-fill h-min max-w-xl rounded-lg shadow-xl dark:shadow-gray-800"
+                                src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                                alt={`Poster of ${movie}`}
+                            />
+                            <div className="flex flex-col ml-20 space-y-5">
+                                <p className="logo body text-6xl">{title}</p>
+                                <div className="flex flex-row space-x-2">
+                                    {genres &&
+                                        genres.map(({ id, name }) => {
+                                            return <Badge content={name} />;
+                                        })}
+                                    <span> • {release_date.slice(0, 4)}</span>
+                                </div>
+                                <p className="body pr-24">{overview}</p>
+                                <div className="flex flex-row space-x-5">
+                                    <button className="btn-add w-52">
+                                        &#9658; Watch Now
+                                    </button>
+                                    <button className="btn-add w-52">
+                                        &#x2b; Add to list
+                                    </button>
+                                </div>
                             </div>
-                            <p className="body pr-24">{overview}</p>
-                            <div className="flex flex-row space-x-5">
-                                <button className="btn-add w-52">
-                                    &#9658; Watch Now
-                                </button>
-                                <button className="btn-add w-52">
-                                    &#x2b; Add to list
-                                </button>
+                        </div>
+                        <div className="items-center mt-20 mx-20">
+                            <p className="logo body text-5xl">Similar Movies</p>
+                            <div className="flex h-2/5 flex-row flex-shrink-0 space-x-10 overflow-scroll">
+                                {/* {similarMovies.results.map(
+                                    ({ title, poster_path, release_date, id }) => {
+                                        return (
+                                            <SuggestedMovie
+                                                title={title}
+                                                poster_path={poster_path}
+                                                release_date={release_date.slice(
+                                                    0,
+                                                    4
+                                                )}
+                                                key={id}
+                                            />
+                                        );
+                                    }
+                                )} */}
+                                <SuggestedMovie title="Inception" poster_path="/oYuLEt3zVCKq57qu2F8dT7NIa6f.jpg" release_date="2014-09-44" />
                             </div>
                         </div>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     );
