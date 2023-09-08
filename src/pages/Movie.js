62e411bbcdf01e2bf4a6e170f767f6f5d6f7ded7
Shantanu Wable <shantanuwable2003@gmail.com>
2023-09-08 23:47:28 +0530

    Added backdrop and badges

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -5,6 +5,7 @@ import NavBar from "../components/NavBar";
 import Search from "../components/Search";
 import Badge from "../components/Badge";
 import Numresults from "../utils/Numresults";
+import axios from "axios";
 
 function Movie() {
     const [query, setQuery] = useState("");
@@ -22,10 +23,21 @@ function Movie() {
         id,
         poster_path,
         vote_average,
+        backdrop_path,
         release_date,
         overview,
         runtime,
+        genres,
     } = movie;
+
+    let backdropURL = `https://image.tmdb.org/t/p/original${backdrop_path}`;
+
+    const backgroundStyle = {
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.6)), url(${backdropURL})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+    };
 
     useEffect(
         function () {
@@ -34,6 +46,20 @@ function Movie() {
                 const res = await fetch(
                     `https://api.themoviedb.org/3/movie/${selectedID}?api_key=${API_KEY}`
                 );
+
+                // const url =
+                //     `https://api.themoviedb.org/3/movie/${selectedID}?language=en-US`;
+                // const options = {
+                //     method: "GET",
+                //     headers: {
+                //         accept: "application/json",
+                //         Authorization:
+                //             `Bearer ${API_KEY}`,
+                //     },
+                // };
+
+                // const res = await fetch(url, options)
+
                 const data = await res.json();
                 // console.log(data);
                 setMovie(data);
@@ -51,7 +77,7 @@ function Movie() {
                 {/* <Numresults movies={movies} /> */}
             </NavBar>
             <div className="main">
-                <div className="movieBox">
+                <div className="movieBox" style={backgroundStyle}>
                     <div className="flex flex-row items-center mt-20 mx-20">
                         <img
                             className="object-fill h-min max-w-xl rounded-lg shadow-xl dark:shadow-gray-800"
@@ -60,10 +86,13 @@ function Movie() {
                         />
                         <div className="flex flex-col ml-20 space-y-5">
                             <p className="logo body text-6xl">{title}</p>
-                            <p className="body pr-24">{overview}</p>
-                            <div>
-                            <Badge content="hello"/>
+                            <div className="flex flex-row space-x-2">
+                                {genres &&
+                                    genres.map(({ id, name }) => {
+                                        return <Badge content={name} />;
+                                    })}
                             </div>
+                            <p className="body pr-24">{overview}</p>
                             <div className="flex flex-row space-x-5">
                                 <button className="btn-add w-52">
                                     &#9658; Watch Now
