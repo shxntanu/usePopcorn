8ff8aa7a44f92e83e14a25dca748e514ce19e1a2
Shantanu Wable <shantanuwable2003@gmail.com>
2023-09-08 23:47:16 +0530

    Completed the badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,6 +1,6 @@
 function Badge({ content }) {
     return (
-        <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-l font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">
+        <span className="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-l font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">
             {content}
         </span>
     );
