dff4e90b2bebc6d93a0664ec24afe5f64a048d75
Shantanu <shantanuwable2003@gmail.com>
2023-09-09 10:19:27 +0530

    Added hover effect

--- src/components/SuggestedMovie.js
+++ src/components/SuggestedMovie.js
@@ -1,8 +1,8 @@
 function SuggestedMovie({ title, poster_path, release_date }) {
     return (
-        <div className=" max-w-max">
+        <div className="shrink-0 max-w-fit">
             <img
-                className="object-fill w-fit shadow-xl dark:shadow-gray-800"
+                className="transform transition duration-500 hover:scale-110 w-auto object-fill rounded-lg shadow-xl dark:shadow-gray-800"
                 src={`https://image.tmdb.org/t/p/w500${poster_path}`}
                 alt={`Poster of ${title}`}
             />
