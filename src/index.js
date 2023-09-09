8c2e2f5821378b5bd652ccf1cf913e30af24c79b
Shantanu <shantanuwable2003@gmail.com>
2023-09-09 12:59:43 +0530

    Added route Parameter

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ export default function App() {
             <Router>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/movie" element={<Movie />} />
+                    <Route path="/movie/:selectedID" element={<Movie />} />
                 </Routes>
             </Router>
         </div>
