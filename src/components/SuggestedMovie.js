e2ce0c890f5dc427a95879fe539eae1a01cb32fe
Shantanu <shantanuwable2003@gmail.com>
2023-09-09 13:00:24 +0530

    Fixed image sizing and added onClick property

--- src/components/SuggestedMovie.js
+++ src/components/SuggestedMovie.js
@@ -1,12 +1,22 @@
-function SuggestedMovie({ title, poster_path, release_date }) {
+import { useNavigate } from "react-router-dom";
+
+function SuggestedMovie({ title, poster_path, release_date, selectedID }) {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate(`/movie/${selectedID}`, {
+            state: {
+                selectedID: selectedID,
+            },
+        });
+    };
     return (
-        <div className="shrink-0 max-w-fit">
+        <div className="transform transition duration-500 hover:scale-110 p-5 shrink-0 max-w-xl max-h-min" onClick={handleClick}>
             <img
-                className="transform transition duration-500 hover:scale-110 w-auto object-fill rounded-lg shadow-xl dark:shadow-gray-800"
+                className=" w-auto min-w-fit max-w-xl max-h-96 rounded-lg shadow-xl dark:shadow-gray-800"
                 src={`https://image.tmdb.org/t/p/w500${poster_path}`}
                 alt={`Poster of ${title}`}
             />
-            <div>
+            <div className="pt-3 max-w-xs">
                 <span>{title}</span>
                 <p>{release_date}</p>
             </div>
